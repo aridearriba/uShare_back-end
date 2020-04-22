33bdaea420cae2d9a1c23d19559a7e23bff9fa3f
Ari de Arriba <aridease@gmail.com>
2020-04-22 20:29:30 +0200

    Getters and setters

--- src/main/java/com/example/sardapp/entities/User.java
+++ src/main/java/com/example/sardapp/entities/User.java
@@ -40,16 +40,199 @@ public class User
     {
         return email;
     }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
     public String getPassword()
     {
         return password;
     }
+
     public void setPassword(String password)
     {
         this.password = password;
     }
+
+    public byte[] getImage()
+    {
+        return image;
+    }
+
     public void setImage(byte[] image)
     {
         this.image = image;
+    }
+
+    public String getImageType()
+    {
+        return imageType;
+    }
+
+    public void setImageType(String imageType)
+    {
+        this.imageType = imageType;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getSurname()
+    {
+        return surname;
+    }
+
+    public void setSurname(String surname)
+    {
+        this.surname = surname;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public Integer getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getBirthday()
+    {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday)
+    {
+        this.birthday = birthday;
+    }
+
+    public Boolean getVehicle()
+    {
+        return vehicle;
+    }
+
+    public void setVehicle(Boolean vehicle)
+    {
+        this.vehicle = vehicle;
+    }
+
+    public String getComarca()
+    {
+        return comarca;
+    }
+
+    public void setComarca(String comarca)
+    {
+        this.comarca = comarca;
+    }
+
+    public Boolean getAplecs()
+    {
+        return aplecs;
+    }
+
+    public void setAplecs(Boolean aplecs)
+    {
+        this.aplecs = aplecs;
+    }
+
+    public Boolean getBallades()
+    {
+        return ballades;
+    }
+
+    public void setBallades(Boolean ballades)
+    {
+        this.ballades = ballades;
+    }
+
+    public Boolean getConcerts()
+    {
+        return concerts;
+    }
+
+    public void setConcerts(Boolean concerts)
+    {
+        this.concerts = concerts;
+    }
+
+    public Boolean getConcursos()
+    {
+        return concursos;
+    }
+
+    public void setConcursos(Boolean concursos)
+    {
+        this.concursos = concursos;
+    }
+
+    public Boolean getCursets()
+    {
+        return cursets;
+    }
+
+    public void setCursets(Boolean cursets)
+    {
+        this.cursets = cursets;
+    }
+
+    public Boolean getAltres()
+    {
+        return altres;
+    }
+
+    public void setAltres(Boolean altres)
+    {
+        this.altres = altres;
+    }
+
+    public Boolean getComptarRepartir()
+    {
+        return comptarRepartir;
+    }
+
+    public void setComptarRepartir(Boolean comptarRepartir)
+    {
+        this.comptarRepartir = comptarRepartir;
+    }
+
+    public Boolean getCompetidor()
+    {
+        return competidor;
+    }
+
+    public void setCompetidor(Boolean competidor)
+    {
+        this.competidor = competidor;
+    }
+
+    public Boolean getCoblaCompeticio()
+    {
+        return coblaCompeticio;
+    }
+
+    public void setCoblaCompeticio(Boolean coblaCompeticio)
+    {
+        this.coblaCompeticio = coblaCompeticio;
     }
 }
